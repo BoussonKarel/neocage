1b9bff738412f1301c412c32d430d1c5825a392a
Karel <karel.bousson@student.howest.be>
2021-01-04 15:10:10 +0100

    Added "Add gamemode"

---
+++ NCFunctions/Entities/GamemodeEntity.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.Azure.Cosmos.Table;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace NCFunctions.Entities
+{
+    public class GamemodeEntity : TableEntity
+    {
+        public GamemodeEntity()
+        {
+
+        }
+
+        public GamemodeEntity(string id, string desc, int dur, bool rel)
+        {
+            Id = id;
+            Description = desc;
+            Duration = dur;
+            Released = rel;
+            RowKey = Id;
+            PartitionKey = Released.ToString();
+        }
+
+        public string Id { get; set; }
+        public string Description { get; set; }
+        public int Duration { get; set; }
+        public bool Released { get; set; }
+    }
+}
--- NCFunctions/Repositories/GamemodeRepository.cs
+++ NCFunctions/Repositories/GamemodeRepository.cs
@@ -1,10 +1,42 @@
-﻿using System;
+﻿using Microsoft.Azure.Cosmos.Table;
+using NCFunctions.Entities;
+using NCFunctions.Helpers;
+using NCFunctions.Models;
+using System;
 using System.Collections.Generic;
 using System.Text;
+using System.Threading.Tasks;
 
 namespace NCFunctions.Repositories
 {
     public static class GamemodeRepository
     {
+        public static async Task<List<Gamemode>> GetGamemodesAsync() {
+            List<Gamemode> gamemodes = new List<Gamemode>();
+
+            return gamemodes;
+        }
+
+        public static async Task<Gamemode> AddGamemodeAsync(Gamemode gm)
+        {
+            try
+            {
+                // Make Entity of it
+                GamemodeEntity gamemodeEntity = new GamemodeEntity(gm.Id, gm.Description, gm.Duration, gm.Released);
+
+                CloudTable gamemodeTable = await StorageHelper.GetCloudTable("gamemodes");
+
+                TableOperation insertOperation = TableOperation.Insert(gamemodeEntity);
+
+                await gamemodeTable.ExecuteAsync(insertOperation);
+
+                return gm;
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+            
+        }
     }
 }
