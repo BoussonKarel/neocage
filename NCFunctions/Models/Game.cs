9327d14686331b144bb32be0d98da5baa6567e25
Karel <karel.bousson@student.howest.be>
2021-01-06 11:11:29 +0100

    Added JSON properties

--- NCFunctions/Models/Game.cs
+++ NCFunctions/Models/Game.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Newtonsoft.Json;
+using System;
 using System.Collections.Generic;
 using System.Text;
 
@@ -6,9 +7,13 @@ namespace NCFunctions.Models
 {
     public class Game
     {
+        [JsonProperty("id")]
         public Guid Id { get; set; }
+        [JsonProperty("gamemode")]
         public string GamemodeId { get; set; }
+        [JsonProperty("duration")]
         public int? Duration { get; set; }
+        [JsonProperty("score")]
         public int Score { get; set; }
     }
 }
