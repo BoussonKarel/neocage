c4908016d1ca5abf0862b1f4a287b830e932c783
Karel <karel.bousson@student.howest.be>
2021-01-06 18:01:42 +0100

    changed game_stop to game_end in device

--- testdevice/testdevice.py
+++ testdevice/testdevice.py
@@ -61,6 +61,7 @@ def twin_patch_handler(patch):
     # print(f"Threshold update: {threshold}")
 
 def testmenu():
+    global current_game
     print("Maak een keuze: ")
     print("1) Stuur score update")
     print("2) Stop game")
@@ -84,9 +85,14 @@ def testmenu():
     if (keuze == 2):
         # END GAME
         if (current_game):
+            current_game = None
+
+            print("Game is gestopt")
+            print("Dit verzenden naar Cloud / Backend...")
+
             # Save in IoTMessage format
             data = dict()
-            data["type"] = "game_stop"
+            data["type"] = "game_end"
             data["payload"] = json.dumps(current_game)
             
             # Send game update
