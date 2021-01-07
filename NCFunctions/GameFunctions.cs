be6c728a3dd58eddd994d0d8d3243ebaee97c5b4
Karel <karel.bousson@student.howest.be>
2021-01-07 11:18:01 +0100

    Added get (all) played games, update played game

--- NCFunctions/GameFunctions.cs
+++ NCFunctions/GameFunctions.cs
@@ -10,6 +10,7 @@ using Newtonsoft.Json;
 using NCFunctions.Helpers;
 using NCFunctions.Models;
 using NCFunctions.Repositories;
+using System.Collections.Generic;
 
 namespace NCFunctions
 {
@@ -66,7 +67,7 @@ namespace NCFunctions
 
         [FunctionName("GetCurrentGame")]
         public static async Task<IActionResult> GetCurrentGame(
-            [HttpTrigger(AuthorizationLevel.Anonymous, "get", Route = "game")] HttpRequest req,
+            [HttpTrigger(AuthorizationLevel.Anonymous, "get", Route = "games/current")] HttpRequest req,
             ILogger log)
         {
             try
@@ -75,6 +76,64 @@ namespace NCFunctions
                 Game CurrentGame = await GameRepository.GetCurrentGameAsync();
 
                 return new OkObjectResult(CurrentGame);
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+        }
+
+        [FunctionName("GetGamesWithScore")]
+        public static async Task<IActionResult> GetGamesWithScore(
+            [HttpTrigger(AuthorizationLevel.Anonymous, "get", Route = "games")] HttpRequest req,
+            ILogger log)
+        {
+            try
+            {
+                // User wants to get all games with a username / score
+                List<Game> gamesWithScore = await GameRepository.GetGamesAsync(true);
+
+                return new OkObjectResult(gamesWithScore);
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+        }
+
+        [FunctionName("GetAllGames")]
+        public static async Task<IActionResult> GetAllGames(
+            [HttpTrigger(AuthorizationLevel.Anonymous, "get", Route = "games/all")] HttpRequest req,
+            ILogger log)
+        {
+            try
+            {
+                // User wants to get all the games
+                List<Game> games = await GameRepository.GetGamesAsync(false);
+
+                return new OkObjectResult(games);
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+        }
+
+        [FunctionName("UpdateUsernameForGame")]
+        public static async Task<IActionResult> UpdateUsernameForGame(
+            [HttpTrigger(AuthorizationLevel.Anonymous, "put", Route = "games")] HttpRequest req,
+            ILogger log)
+        {
+            try
+            {
+                // User wants to add his username to his score
+                string requestBody = await new StreamReader(req.Body).ReadToEndAsync();
+                Game selectedGame = JsonConvert.DeserializeObject<Game>(requestBody);
+
+                // User wants to get all the games
+                Game game = await GameRepository.UpdateGameAsync(selectedGame);
+
+                return new OkObjectResult(game);
             }
             catch (Exception ex)
             {
