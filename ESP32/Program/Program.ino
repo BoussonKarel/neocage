fd1392fa23e461f8af32f93606b034dbf240ebba
Nico Van Hevel <nicovanhevel@gmail.com>
2021-01-11 17:36:56 +0100

    Optimalizations in code
    Optimalizations in code

--- ESP32/Program/Program.ino
+++ ESP32/Program/Program.ino
@@ -22,30 +22,25 @@ Adafruit_VL53L0X lox3 = Adafruit_VL53L0X();
 Adafruit_VL53L0X sensors[] = {lox1, lox2, lox3};
 //Adafruit_VL53L0X sensors[] = {lox1, lox2, lox3, lox4};
 
-// this holds the measurement
-VL53L0X_RangingMeasurementData_t measure1;
-VL53L0X_RangingMeasurementData_t measure2;
-VL53L0X_RangingMeasurementData_t measure3;
-//VL53L0X_RangingMeasurementData_t measure4;
-
-//VL53L0X_RangingMeasurementData_t measures[] = {measure1,measure2,measure3};
-
 #define LED_PIN 5
 #define JEWEL_COUNT 3
 int LED_COUNT = JEWEL_COUNT * 7;
 
-int sensitivity = 50;
+int sensitivity = 75;
+int lastGoal;
 
 Adafruit_NeoPixel leds(LED_COUNT, LED_PIN, NEO_GRB + NEO_KHZ800);
 
-/*
-    Reset all sensors by setting all of their XSHUT pins low for delay(10), then set all XSHUT high to bring out of reset
-    Keep sensor #1 awake by keeping XSHUT pin high
-    Put all other sensors into shutdown by pulling XSHUT pins low
-    Initialize sensor #1 with lox.begin(new_i2c_address) Pick any number but 0x29 and it must be under 0x7F. Going with 0x30 to 0x3F is probably OK.
-    Keep sensor #1 awake, and now bring sensor #2 out of reset by setting its XSHUT pin high.
-    Initialize sensor #2 with lox.begin(new_i2c_address) Pick any number but 0x29 and whatever you set the first sensor to
-*/
+bool activeSensors[3] = { false, false, false };
+int lastValue[3] = {0,0,0};
+int doneRondo[3] = {false,false,false};
+
+
+void toggleSensor(int index) {
+  bool waarde = activeSensors[index];
+  activeSensors[index] = !waarde;
+}
+
 void setID() {
   // all reset
   digitalWrite(SHT_LOX1, LOW);
@@ -108,62 +103,16 @@ void setID() {
   lox3.configSensor(Adafruit_VL53L0X::VL53L0X_SENSE_HIGH_ACCURACY);
   //lox4.configSensor(Adafruit_VL53L0X::VL53L0X_SENSE_HIGH_ACCURACY);
 
-
+  sensors[0] = lox1;
+  sensors[1] = lox2;
+  sensors[2] = lox3;
+  //sensors[3] = lox4;
+  
   //Led aanzetten
   leds.begin();
   leds.setBrightness(5);
   leds.fill(leds.Color(255, 255, 255), 0, LED_COUNT);
   leds.show();
-}
-
-void read_dual_sensors() {
-
-  lox1.rangingTest(&measure1, false); // pass in 'true' to get debug data printout!
-  lox2.rangingTest(&measure2, false); // pass in 'true' to get debug data printout!
-  lox3.rangingTest(&measure3, false); // pass in 'true' to get debug data printout!
-  //lox4.rangingTest(&measure4, false); // pass in 'true' to get debug data printout!
-
-  // print sensor one reading
-  Serial.print(F("1: "));
-  if (measure1.RangeStatus != 4) {    // if not out of range
-    Serial.print(measure1.RangeMilliMeter);
-  } else {
-    Serial.print(F("Out of range"));
-  }
-
-  Serial.print(F(" "));
-
-  // print sensor two reading
-  Serial.print(F("2: "));
-  if (measure2.RangeStatus != 4) {
-    Serial.print(measure2.RangeMilliMeter);
-  } else {
-    Serial.print(F("Out of range"));
-  }
-
-  Serial.print(F(" "));
-
-  // print sensor two reading
-  Serial.print(F("3: "));
-  if (measure3.RangeStatus != 4) {
-    Serial.print(measure3.RangeMilliMeter);
-  } else {
-    Serial.print(F("Out of range"));
-  }
-
-  Serial.print(F(" "));
-
-  /* print sensor two reading
-  Serial.print(F("4: "));
-  if (measure4.RangeStatus != 4) {
-    Serial.print(measure4.RangeMilliMeter);
-  } else {
-    Serial.print(F("Out of range"));
-  }*/
-
-  Serial.print(F(" "));
-  
-  Serial.println();
 }
 
 void setup() {
@@ -189,97 +138,125 @@ void setup() {
   digitalWrite(SHT_LOX3, LOW);
   //digitalWrite(SHT_LOX4, LOW);
 
-  Serial.println(F("Both in reset mode...(pins are low)"));
-
-
-  Serial.println(F("Starting..."));
   setID();
 }
 
 void setJewel(int jewel, int red, int green, int blue) {
-  jewel = jewel - 1;
   jewel = jewel * 7;
   //Todo Als de jewel groter is dan aantal dan neits aan ofz
   leds.fill(leds.Color(red, green, blue), jewel, 7);
   leds.show();
 }
 
-int readSensor(int sensorId, bool IndexOrNot) {
-  if(IndexOrNot) {
-      sensorId = sensorId - 1;
-      
-      sensors[sensorId].rangingTest(&measurement, false); // pass in 'true'
-      while(measurement.RangeStatus == 4 || measurement.RangeMilliMeter > 8190) {
-        sensors[sensorId].rangingTest(&measurement, false); // pass in 'true'
-      } 
-      Serial.println(String(sensorId) + ": " + measurement.RangeMilliMeter);
-      return measurement.RangeMilliMeter;
-  }
-  else {
-      sensors[sensorId].rangingTest(&measurement, false); // pass in 'true'
-      while(measurement.RangeStatus == 4 || measurement.RangeMilliMeter > 8190) {
-        sensors[sensorId].rangingTest(&measurement, false); // pass in 'true'
-      } 
-      Serial.println(String(sensorId) + ": " + measurement.RangeMilliMeter);
-      return measurement.RangeMilliMeter;
-  }
-
+int readSensor(int sensorId) {
   VL53L0X_RangingMeasurementData_t measurement;
-  sensors[sensorId].rangingTest(&measurement, false); // pass in 'true'
-
-  while(measurement.RangeStatus == 4 || measurement.RangeMilliMeter > 8190) {
+  int milli;
+  do {
     sensors[sensorId].rangingTest(&measurement, false); // pass in 'true'
-  } 
-  Serial.println(measurement.RangeMilliMeter);
-  return measurement.RangeMilliMeter;
+    milli =  measurement.RangeMilliMeter; 
+    //Serial.println(String(sensorId) + " raw: " + milli);
+  } while(measurement.RangeStatus == 4 || milli > 8190 || milli <= 1);
+
+  Serial.println(String(sensorId) + " geslaagd: " + milli);
+  return milli;
+}
+
+void checkSensors() {
+  for(int i = 0; i < JEWEL_COUNT; i++) {
+    if(activeSensors[i] == true) {
+      //Meting doen
+      int value = readSensor(i);
+      int valueWithSensitivity = value + sensitivity;
+      if(valueWithSensitivity < lastValue[i]) {
+        Serial.println("GOAAAAAAAAAAAAAAAAAAAAAAAAL");
+        //er is gescoord
+        toggleSensor(i);
+        //return true als er gescoord is
+        //lastValue[i] = value;
+        doneRondo[i] = true;
+        lastGoal = i;
+        for(int i = 0; i < JEWEL_COUNT; i++) {
+          lastValue[i] = 0;
+        }
+      }
+      else {
+        lastValue[i] = value;
+      }
+    }
+  }
+}
+
+void theRondo() {
+  leds.fill(leds.Color(255, 0, 0), 0, LED_COUNT);
+  leds.show();
+  unsigned long startMillis = millis();
+
+  //alle sensors aanzetten
+  for(int i = 0; i < JEWEL_COUNT; i++) {
+    toggleSensor(i);
+    doneRondo[i] = false;
+  }
+  
+  while(doneRondo[0] == false || doneRondo[1] == false ||doneRondo[2] == false) {
+    checkSensors();
+    for(int j = 0; j < JEWEL_COUNT; j++) {
+      if(activeSensors[j]==false) {
+        setJewel(j, 0,255,0);
+      }
+    }
+  }
+  Serial.println("The rondo done");
+  for(int i = 0; i < JEWEL_COUNT; i++) {
+    lastValue[i] = 0;
+  }
+   
+  leds.fill(leds.Color(255, 255, 255), 0, LED_COUNT);
+  leds.show();
+  delay(1000);
+  leds.fill(leds.Color(255, 0, 0), 0, LED_COUNT);
+  leds.show();
+  delay(1000);
+  leds.fill(leds.Color(255, 255, 255), 0, LED_COUNT);
+  leds.show();
+  delay(1000);  
 }
 
 void quickyTricky(int duration) {
-  //duration * 1000 want komt als secondenbinnen
-  //leds resetten
+  duration = duration * 1000; //Van ms naar seconden
   leds.fill(leds.Color(0, 0, 255), 0, LED_COUNT);
   leds.show();
 
   unsigned long previousMillis = millis();
   unsigned long currentMillis = millis();
-
-  int currentLed = 1;
-  int previousLed = 1;
-
-  int value = readSensor(currentLed);
-  int laatste = readSensor(currentLed);
+  
+  int maxNum = JEWEL_COUNT;
+  int randNumber;
+  
   while (currentMillis - previousMillis <= duration) {
-    //Als er nog geen {duration} seconden zijn gepasseerd ->spelen
-    //Licht 1 op rood en wachten op een goal
-    setJewel(currentLed, 255, 0, 0);
-    //Wachten op goal van sensor 1
-    //Serial.println("Value: " + String(readSensor(currentLed)));
-    value = readSensor(currentLed,1) + sensitivity;
-    //Serial.println("Value + 30: " + String(value));
-    //Serial.println("Laatste: " +  String(laatste));
-    if(value < laatste ) {
-      //Er is gescoord
-      //huidige led op groen, volgende willekeurige led op rood
-      //na enkele seconden groene led terug op wit
-      //Doorsturen backend
-      Serial.println("GOAAAAAAAAAAAAAAAAAAAL");
-      previousLed = currentLed;
-      //random volgende kiezen uit jewel amount -> mag niet de vorige zijn
-      int maxNum = JEWEL_COUNT +1;
-      int randNumber = previousLed;
-      do {
-        randNumber =random(1,maxNum);
-      }while(randNumber == previousLed);
-      currentLed = randNumber;
-      setJewel(previousLed,0,0,255);
-      delay(10);
-      setJewel(currentLed,255,0,0);
-      //TODO: Score bijhouden
-      //Doorsturen IOTHUB
+    //Checken of er een sensor aanstaat -> nee? -> random sensor aan
+    if(activeSensors[0] == true || activeSensors[1] == true ||activeSensors[2] == true) {
+      checkSensors();
     }
-    laatste = value - sensitivity;   
+    else {
+      do {
+        randNumber =random(0,maxNum);
+      }while(randNumber == lastGoal);
+      activeSensors[randNumber] = true;
+      Serial.println("Random number " + String(randNumber));
+      //Led aanzetten
+      setJewel(lastGoal,0,0,255);      
+      setJewel(randNumber,255,0,0);
+    }
     currentMillis = millis();
   }
+  //alles uit
+
+  for(int i = 0; i < JEWEL_COUNT; i++) {
+    activeSensors[i] = false;
+    lastValue[i] = 0;
+  }
+  
+  //Serial.println("Game score " + String(currentScore)); 
   leds.fill(leds.Color(255, 255, 255), 0, LED_COUNT);
   leds.show();
   delay(1000);
@@ -291,6 +268,9 @@ void quickyTricky(int duration) {
 }
 
 void loop() {
-  quickyTricky(60000);
-  delay(1000);
+  //quickyTricky(30);
+  theRondo();
+  //read_dual_sensors();
+  //delay(2000);
+
 }
